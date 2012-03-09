ea108fbcb0c0cf7b196a88a3e3005b903fc28f7e
Olga Fadeeva <belotapo4kina@gmail.com>
2012-03-10 01:18:04 +0300

    + before_filter в pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  #before_filter :authenticate_user!, :except => [:home, :contact]
+  before_filter :authenticate_user!, :except => [:home, :contact]
 
   def home
     @title = "Home"
