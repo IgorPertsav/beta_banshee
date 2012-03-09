a5d408d8a8cf6d917b401f733f61eb73bd61999f
Igor Pertsav <pertsavigorstray@gmail.com>
2012-03-09 11:17:50 +0300

    First try

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetaBanshee::Application.config.secret_token = 'f4be570b122622bd147476fd9b0cce870dd2dc143ef25b523adfae38c0c40e3c5ebaffb980da14e88c0b7523ba355e00ab3145d028e833617eff4bcc86cfa1e0'
